22733c57b73b791cb80054bbb05d1544d888ebb0
Николай <@DESKTOP-BMT9AVM>
2023-02-01 19:55:33 +0300

    Добавьте файлы проекта.

---
+++ RPM-1.7/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace RPM_Novikov_1._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            var num1 = Console.ReadLine();
+            Console.WriteLine("Введите второе число:");
+            var num2 = Console.ReadLine();
+            Console.WriteLine("Введите третье число:");
+            var num3 = Console.ReadLine();
+            Console.WriteLine(num1 + "  " + num2 + "  " + num3);
+        }
+    }
+}
